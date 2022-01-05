352fa8e22af1c8bd8ccb9d880bf4e0c55efbf2fd
OldMateTyler <83928256+OldMateTyler@users.noreply.github.com>
2022-01-05 17:29:05 +1100

    Add files via upload

---
+++ YTT/script.js
@@ -0,0 +1,57 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+
+var flip1 = document.getElementById("toggle-card");
+flip1.onclick = function(){
+    var className = document.getElementById("toggle-me");
+    className.classList.toggle('flip-toggle');
+}
+var returnElement1 = document.getElementById("return-card");
+returnElement1.onclick = function(){
+    var className = document.getElementById("toggle-me");
+    className.classList.toggle('flip-toggle');
+}
+
+var flip2 = document.getElementById("toggle-card-2");
+flip2.onclick = function(){
+    var className = document.getElementById("toggle-me-2");
+    className.classList.toggle('flip-toggle');
+}
+var returnElement2 = document.getElementById("return-card-2");
+returnElement2.onclick = function(){
+    var className = document.getElementById("toggle-me-2");
+    className.classList.toggle('flip-toggle');
+}
+
+var flip3 = document.getElementById("toggle-card-3");
+flip3.onclick = function(){
+    var className = document.getElementById("toggle-me-3");
+    className.classList.toggle('flip-toggle');
+}
+var returnElement3 = document.getElementById("return-card-3");
+returnElement3.onclick = function(){
+    var className = document.getElementById("toggle-me-3");
+    className.classList.toggle('flip-toggle');
+}
+
+
+window.addEventListener('resize', function(){
+    var flip = document.getElementById("test").offsetHeight;
+    $(".flip-box").css('height', flip + "px");
+
+    var titleheight = document.getElementById("title-img").offsetHeight;
+    $('.titles').css('height',titleheight + 'px');
+});
+window.onload = function(){
+    var flip = document.getElementById("test").offsetHeight;
+    $(".flip-box").css('height', flip + "px");
+
+    var titleheight = document.getElementById("title-img").offsetHeight;
+    $('.titles').css('height',titleheight + 'px');
+};
