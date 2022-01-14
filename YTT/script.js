24e1c76b7c36ed6eec74627730c82a2b9cb0535d
OldMateTyler <tyler.simmonds254@gmail.com>
2022-01-14 17:06:55 +1100

    test mobile scroll

--- YTT/script.js
+++ YTT/script.js
@@ -74,28 +74,17 @@ function openNav(){
       let progressWidth = (scrollDistance / (section.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100;
       $("#progress-bar").css("width", progressWidth +"%");
   };
-  window.addEventListener('scroll',animateProgressBar);
-  $(document).ready(function(){
-      // Add smooth scrolling to all links
-      $("a").on('click', function(event) {
-    
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-          // Prevent default anchor click behavior
-          event.preventDefault();
-    
-          // Store hash
-          var hash = this.hash;
-    
-          // Using jQuery's animate() method to add smooth page scroll
-          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-    
-            // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-          });
-        } // End if
-      });
+  const links = document.querySelectorAll(".class-button");
+  clickHandler(links);
+  
+   function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+    alert(href);
+  
+    scroll({
+      top: offsetTop,
+      behavior: "smooth"
     });
+  }
