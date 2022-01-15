273d3360acbbde0c708edca5736568a2ad366e31
OldMateTyler <tyler.simmonds254@gmail.com>
2022-01-15 15:23:52 +1100

    carousel updated working

--- YTT/script.js
+++ YTT/script.js
@@ -18,7 +18,7 @@ function openNav(){
     }
     
   }
-  
+
   
   var flip1 = document.getElementById("toggle-card");
   flip1.onclick = function(){
@@ -52,18 +52,61 @@ function openNav(){
       var className = document.getElementById("toggle-me-3");
       className.classList.toggle('flip-toggle');
   };
+
+  var flip1 = document.getElementById("toggle-card-c");
+  flip1.onclick = function(){
+      var className = document.getElementById("toggle-me-c");
+      className.classList.toggle('flip-toggle');
+  };
+  var returnElement1 = document.getElementById("return-card-c");
+  returnElement1.onclick = function(){
+      var className = document.getElementById("toggle-me-c");
+      className.classList.toggle('flip-toggle');
+  };
+  
+  var flip2 = document.getElementById("toggle-card-2-c");
+  flip2.onclick = function(){
+      var className = document.getElementById("toggle-me-2-c");
+      className.classList.toggle('flip-toggle');
+  };
+  var returnElement2 = document.getElementById("return-card-2-c");
+  returnElement2.onclick = function(){
+      var className = document.getElementById("toggle-me-2-c");
+      className.classList.toggle('flip-toggle');
+  };
+  
+  var flip3 = document.getElementById("toggle-card-3-c");
+  flip3.onclick = function(){
+      var className = document.getElementById("toggle-me-3-c");
+      className.classList.toggle('flip-toggle');
+  };
+  var returnElement3 = document.getElementById("return-card-3-c");
+  returnElement3.onclick = function(){
+      var className = document.getElementById("toggle-me-3-c");
+      className.classList.toggle('flip-toggle');
+  };
   
   
   window.addEventListener('resize', function(){
       var flip = document.getElementById("test").offsetHeight;
       $(".flip-box").css('height', flip + "px");
       $(".flip-box-back").css('height', flip + "px");
+
+      var flip = document.getElementById("test-2").offsetHeight;
+      $(".flip-box-2").css('height', flip + "px");
+      $(".flip-box-back-2").css('height', flip + "px");
+
+      
   
   });
   window.onload = function(){
       var flip = document.getElementById("test").offsetHeight;
       $(".flip-box").css('height', flip + "px");
       $(".flip-box-back").css('height', flip + "px");
+
+      var flip = document.getElementById("test-2").offsetHeight;
+      $(".flip-box-2").css('height', flip + "px");
+      $(".flip-box-back-2").css('height', flip + "px");
   };
   
   const progressBar = document.querySelectorAll("#progress-bar");
@@ -75,27 +118,3 @@ function openNav(){
       $("#progress-bar").css("width", progressWidth +"%");
   };
   window.addEventListener('scroll',animateProgressBar);
-  $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $(".smooth-scroll").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offsetTop
-        }, 800, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-  });
