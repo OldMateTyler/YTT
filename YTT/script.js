be727f4e7422fc0506cf9e4c8d97a28bda8b51c1
OldMateTyler <tyler.simmonds254@gmail.com>
2022-01-15 11:56:33 +1100

    smooth scroll mobile

--- YTT/script.js
+++ YTT/script.js
@@ -75,3 +75,14 @@ function openNav(){
       $("#progress-bar").css("width", progressWidth +"%");
   };
   window.addEventListener('scroll',animateProgressBar);
+
+  const navlink = document.querySelectorAll(".slow-scroll");
+  navlink.forEach(elem => elem.addEventListener("click", smoothscroll));
+  function smoothscroll(event){
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute("href");
+    window.scrollTo({
+      top: document.querySelector(targetId).offsetTop,
+      behavior: "smooth"
+    })
+  }
