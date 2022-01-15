c2f00826da4699c2a587134c66b7119917be8660
OldMateTyler <tyler.simmonds254@gmail.com>
2022-01-15 12:07:08 +1100

    hopefully fix-mobile scroll

--- YTT/script.js
+++ YTT/script.js
@@ -76,13 +76,27 @@ function openNav(){
   };
   window.addEventListener('scroll',animateProgressBar);
 
-  const navlink = document.querySelectorAll(".slow-scroll");
-  navlink.forEach(elem => elem.addEventListener("click", smoothscroll));
-  function smoothscroll(event){
-    event.preventDefault();
-    const targetId = event.currentTarget.getAttribute("href");
-    window.scrollTo({
-      top: document.querySelector(targetId).offsetTop,
-      behavior: "smooth"
-    })
-  }
+  $(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".slow-scroll").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offsetTop
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
